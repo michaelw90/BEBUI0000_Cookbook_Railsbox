2d5bb50fd953c4302ef7039aa07d47643cdca6aa
Fred Thompson <fred.thompson@buildempire.co.uk>
2013-09-06 16:18:58 +0100

    Try gaain!

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,7 @@
 include_recipe 'apt'
 package "libsqlite3-dev"
 include_recipe 'sqlite'
-include_recipe 'newrelic'
+include_recipe 'newrelic-sysmond'
 include_recipe 'databox'
 include_recipe 'rackbox'
 include_recipe 'imagemagick'
