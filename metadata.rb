836047fcbda0e5ad58df0c33be442454fd533abd
Fred Thompson <fred.thompson@buildempire.co.uk>
2013-09-06 15:43:16 +0100

    Ergh.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email 'fred.thompson@buildempire.co.uk'
 license          'Apache 2.0'
 description      'Ruby on Rails server, ready for Capistrano deployment.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
+version          '0.1.4'
 
 recipe 'cookbook_railsbox', 'Ruby on Rails server, ready for Capistrano deployment.'
 
