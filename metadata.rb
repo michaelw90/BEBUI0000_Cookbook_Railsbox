229defc538559c574b42a980a42d17befc6e3b47
Fred Thompson <fred.thompson@buildempire.co.uk>
2013-09-06 15:33:09 +0100

    Added a different version of newrelic.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email 'fred.thompson@buildempire.co.uk'
 license          'Apache 2.0'
 description      'Ruby on Rails server, ready for Capistrano deployment.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 
 recipe 'cookbook_railsbox', 'Ruby on Rails server, ready for Capistrano deployment.'
 
@@ -11,6 +11,8 @@ recipe 'cookbook_railsbox', 'Ruby on Rails server, ready for Capistrano deployme
   supports os
 end
 
-%w{build-essential sqlite newrelic databox rackbox imagemagick}.each do |cb|
+%w{build-essential sqlite databox rackbox imagemagick}.each do |cb|
   depends cb
 end
+
+depends 'newrelic', git: 'git@github.com:heavywater/chef-newrelic.git'
