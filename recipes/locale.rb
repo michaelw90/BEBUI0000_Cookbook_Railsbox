165940b078d9d12feef5eeecfaefac002bc2399f
Fred Thompson <fred.thompson@buildempire.co.uk>
2014-04-16 17:16:18 +0100

    Updated the locales again.

--- recipes/locale.rb
+++ recipes/locale.rb
@@ -7,14 +7,14 @@ execute 'Update locale' do
   lang = 'en_GB.utf8'
   language = 'en_GB:en'
   lc_all = 'en_GB.utf8'
-  command_string = "update-locale LANG=#{lang}"
+  command_string = "sudo update-locale LANG=#{lang}"
   command_string << " LANGUAGE=#{language}" unless language.nil?
   command_string << " LC_ALL=#{lc_all}" unless lc_all.nil?
   Chef::Log.info("locale command is #{command_string.inspect}")
   command command_string
-  command "locale-gen #{lang}"
+  command "sudo locale-gen #{lang}"
   Chef::Log.info("locale-gen #{lang}")
-  command "dpkg-reconfigure locales"
+  command "sudo dpkg-reconfigure locales"
 end
 
 ENV['LANGUAGE'] = ENV['LANG'] = ENV['LC_ALL'] = "en_GB.utf8"
