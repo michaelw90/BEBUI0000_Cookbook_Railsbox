78fff885c013b2120a4236364d31eca20b02b08f
Fred Thompson <fred.thompson@buildempire.co.uk>
2014-04-16 21:52:36 +0100

    Changed to GB.

--- recipes/postpostgresql.rb
+++ recipes/postpostgresql.rb
@@ -9,7 +9,7 @@ execute "Psql template1 to UTF8" do
 echo "
 UPDATE pg_database SET datistemplate = FALSE WHERE datname = 'template1';
 DROP DATABASE template1;
-CREATE DATABASE template1 WITH TEMPLATE = template0 ENCODING = 'UNICODE' LC_CTYPE='en_US.utf8'      LC_COLLATE='en_US.utf8';
+CREATE DATABASE template1 WITH TEMPLATE = template0 ENCODING = 'UNICODE' LC_CTYPE='en_GB.utf8'      LC_COLLATE='en_GB.utf8';
 UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template1';
 \\c template1
 VACUUM FREEZE;" | psql postgres -t
