d79412e17aed5ed9d3d499d617c7133bc1cca509
Fred Thompson <fred.thompson@buildempire.co.uk>
2015-01-23 11:26:52 +0000

    Attempted to update the railsbox, as I left a conflict in.

--- recipes/rackspace.rb
+++ recipes/rackspace.rb
@@ -17,15 +17,12 @@ if node['cookbook_railsbox']['rackspace']
 
     if rackspace_monitoring == true
 
-<<<<<<< HEAD
-=======
       #gem_package "fog" do
       #action :install
       #end
 
       #require 'fog'
 
->>>>>>> release/0.4.5
       # Calculate default values
       # Critical at x4 CPU count
       cpu_critical_threshold = (node['cpu']['total'] * 4)
@@ -34,76 +31,6 @@ if node['cookbook_railsbox']['rackspace']
 
       # Define our monitors
       node.set['rackspace_cloudmonitoring']['monitors'] = {
-<<<<<<< HEAD
-          'cpu' =>  { 'type' => 'agent.cpu', },
-          'load' => { 'type'  => 'agent.load_average',
-                      'alarm' => {
-                          'notification_plan_id' => 'npTechnicalContactsEmail',
-                          'CRITICAL' => {
-                              'conditional' => "metric['5m'] > #{cpu_critical_threshold}",
-                          },
-                          'WARNING'  => {
-                              'conditional' => "metric['5m'] > #{cpu_warning_threshold}",
-                          },
-                      },
-          },
-
-          'disk' => {
-              'type' => 'agent.disk',
-              'details' => { 'target' => '/dev/xvda1'},
-          },
-          'root_filesystem' => {
-              'type' => 'agent.filesystem',
-              'details' => { 'target' => '/'},
-          },
-
-          'web_check' => {
-              'type' => 'remote.http',
-              'target_hostname' => node['fqdn'],
-              'monitoring_zones_poll' => [
-                  'mzdfw',
-                  'mziad',
-                  'mzord'
-              ],
-              'details' => {
-                  "url" => "http://#{node['ipaddress']}/",
-                  "method" => "GET"
-              }
-          }
-      }
-      include_recipe "rackspace_cloudmonitoring::monitors"
-
-    end
-
-    if rackspace_backup == true
-
-      node.set['rackspace_cloudbackup']['backups_defaults']['cloud_notify_email'] = 'fred.thompson@buildempire.co.uk'
-      node.set['rackspace_cloudbackup']['backups'] =
-          [
-              { location: '/home' }
-      ]
-      include_recipe 'rackspace_cloudbackup'
-
-      if node.attribute?("databox")
-        if node["databox"].attribute?("databases")
-          if node["databox"]["databases"]["mysql"]
-            cron 'database-dump' do
-              day '*'
-              hour '2'
-              minute '0'
-              month '*'
-              weekday '*'
-              user 'root'
-              mailto 'fred.thompson@buildempire.co.uk'
-              command "mysqldump --user=root --password='#{node['databox']['db_root_password']}' --all-databases --skip-lock-tables | gzip > #{node["appbox"]["apps_dir"]}/database_latest.sql.gz"
-              action :create
-            end
-          end
-          if node["databox"]["databases"]["postgresql"]
-          end
-        end
-      end
-=======
         'cpu' =>  { 'type' => 'agent.cpu', },
         'load' => { 'type'  => 'agent.load_average',
                     'alarm' => {
@@ -152,7 +79,6 @@ if node['cookbook_railsbox']['rackspace']
           { location: '/home' }
       ]
       include_recipe 'rackspace_cloudbackup'
->>>>>>> release/0.4.5
 
     end
 
