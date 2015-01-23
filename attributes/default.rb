e765cb8c5dc2df09d284a9389e1b896d637f3996
Fred Thompson <fred.thompson@buildempire.co.uk>
2015-01-23 11:15:02 +0000

    Finish ConversionToUseBECookbooks

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,13 +1,6 @@
-default["databox"]["databases"] = {}
-
 default['cookbook_railsbox']['rackspace']['username'] = ''
 default['cookbook_railsbox']['rackspace']['api_key'] = ''
-default['cookbook_railsbox']['rackspace']['auth_region'] = 'uk'
-default['cookbook_railsbox']['rackspace']['endpoint'] = 'lon'
-default['cookbook_railsbox']['rackspace']['container_name'] = ''
-default['cookbook_railsbox']['rackspace']['daily_backup_hour'] = '23'
-default['cookbook_railsbox']['rackspace']['daily_backup_minute'] = '0'
-default['cookbook_railsbox']['rackspace']['daily_database_backup_hour'] = '22'
-default['cookbook_railsbox']['rackspace']['daily_database_backup_minute'] = '0'
+default['cookbook_railsbox']['rackspace']['monitoring'] = true
+default['cookbook_railsbox']['rackspace']['backup'] = true
 
 default['build-essential']['compile_time'] = true
--- recipes/rackspace.rb
+++ recipes/rackspace.rb
@@ -7,70 +7,78 @@ if node['cookbook_railsbox']['rackspace']
 
   rackspace_username = node['cookbook_railsbox']['rackspace']['username']
   rackspace_api_key = node['cookbook_railsbox']['rackspace']['api_key']
-  rackspace_auth_region = node['cookbook_railsbox']['rackspace']['auth_region']
-  rackspace_endpoint = node['cookbook_railsbox']['rackspace']['endpoint']
-  rackspace_container_name = node['cookbook_railsbox']['rackspace']['container_name']
-  daily_backup_hour = node['cookbook_railsbox']['rackspace']['daily_backup_hour']
-  daily_backup_minute = node['cookbook_railsbox']['rackspace']['daily_backup_minute']
-  daily_database_backup_hour = node['cookbook_railsbox']['rackspace']['daily_database_backup_hour']
-  daily_database_backup_minute = node['cookbook_railsbox']['rackspace']['daily_database_backup_minute']
+  rackspace_monitoring = node['cookbook_railsbox']['rackspace']['monitoring']
+  rackspace_backup = node['cookbook_railsbox']['rackspace']['backup']
 
-  if rackspace_username != '' && rackspace_api_key != '' && rackspace_auth_region != ''
+  if rackspace_username != '' && rackspace_api_key
 
-    gem_package "fog" do
-      action :install
+    node.set['rackspace']['cloud_credentials']['username'] = rackspace_username
+    node.set['rackspace']['cloud_credentials']['api_key'] = rackspace_api_key
+
+    if rackspace_monitoring == true
+
+      #gem_package "fog" do
+      #action :install
+      #end
+
+      #require 'fog'
+
+      # Calculate default values
+      # Critical at x4 CPU count
+      cpu_critical_threshold = (node['cpu']['total'] * 4)
+      # Warning at x2 CPU count
+      cpu_warning_threshold = (node['cpu']['total'] * 2)
+
+      # Define our monitors
+      node.set['rackspace_cloudmonitoring']['monitors'] = {
+        'cpu' =>  { 'type' => 'agent.cpu', },
+        'load' => { 'type'  => 'agent.load_average',
+                    'alarm' => {
+                      'notification_plan_id' => 'npTechnicalContactsEmail',
+                      'CRITICAL' => {
+                        'conditional' => "metric['5m'] > #{cpu_critical_threshold}",
+                      },
+                      'WARNING'  => {
+                        'conditional' => "metric['5m'] > #{cpu_warning_threshold}",
+                      },
+                    },
+        },
+
+        'disk' => {
+          'type' => 'agent.disk',
+          'details' => { 'target' => '/dev/xvda1'},
+        },
+        'root_filesystem' => {
+          'type' => 'agent.filesystem',
+          'details' => { 'target' => '/'},
+        },
+
+        'web_check' => {
+          'type' => 'remote.http',
+          'target_hostname' => node['fqdn'],
+          'monitoring_zones_poll' => [
+            'mzdfw',
+            'mziad',
+            'mzord'
+          ],
+          'details' => {
+            "url" => "http://#{node['ipaddress']}/",
+            "method" => "GET"
+          }
+        }
+      }
+      include_recipe "rackspace_cloudmonitoring::monitors"
+
     end
 
-    require 'fog'
+    if rackspace_backup == true
 
-    node.set['cloud_monitoring']['rackspace_username'] = rackspace_username
-    node.set['cloud_monitoring']['rackspace_api_key'] = rackspace_api_key
-    node.set['cloud_monitoring']['rackspace_auth_region'] = rackspace_auth_region
-    include_recipe 'cloud_monitoring::agent'
-  end
-
-  if rackspace_username != '' && rackspace_api_key != '' && rackspace_endpoint != '' && rackspace_container_name != ''
-
-    node.set['rackspace_cloud_backup']['rackspace_username'] = rackspace_username
-    node.set['rackspace_cloud_backup']['rackspace_apikey'] = rackspace_api_key
-    node.set['rackspace_cloud_backup']['rackspace_endpoint'] = rackspace_endpoint
-
-    node.set['rackspace_cloud_backup']['backup_cron_hour'] = daily_backup_hour
-    node.set['rackspace_cloud_backup']['backup_cron_day'] = '*'
-    node.set['rackspace_cloud_backup']['backup_cron_weekday'] = '*'
-    node.set['rackspace_cloud_backup']['backup_cron_month'] = '*'
-    node.set['rackspace_cloud_backup']['backup_cron_minute'] = daily_backup_minute
-    node.set['rackspace_cloud_backup']['backup_cron_user'] = 'root'
-
-    node.set['rackspace_cloud_backup']['backup_container'] = rackspace_container_name
-    node.set['rackspace_cloud_backup']['cloud_notify_email'] = 'fred.thompson@buildempire.co.uk'
-    node.set['rackspace_cloud_backup']['backup_locations'] = [
-      "/home"
-    ]
-
-    include_recipe 'rackspace-cloud-backup::cloud'
-
-    if daily_database_backup_hour != '' && daily_database_backup_minute != ''
-
-      if node.attribute?("databox")
-        if node["databox"].attribute?("databases")
-          if node["databox"]["databases"]["mysql"]
-            cron 'database-dump' do
-              day '*'
-              hour daily_database_backup_hour
-              minute daily_database_backup_minute
-              month '*'
-              weekday '*'
-              user 'root'
-              mailto 'fred.thompson@buildempire.co.uk'
-              command "mysqldump --user=root --password='#{node['databox']['db_root_password']}' --all-databases --skip-lock-tables | gzip > #{node["appbox"]["apps_dir"]}/database_latest.sql.gz"
-              action :create
-            end
-          end
-          if node["databox"]["databases"]["postgresql"]
-          end
-        end
-      end
+      node.set['rackspace_cloudbackup']['backups_defaults']['cloud_notify_email'] = 'fred.thompson@buildempire.co.uk'
+      node.set['rackspace_cloudbackup']['backups'] =
+        [
+          { location: '/home' }
+      ]
+      include_recipe 'rackspace_cloudbackup'
 
     end
 
