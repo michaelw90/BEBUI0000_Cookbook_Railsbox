3fd669656ce0a0168eb47ad04db8c75ecd4677a9
Michael Wright <michael@wserver.co.uk>
2016-05-16 11:46:59 +0100

    Remove DB Items

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,12 +7,6 @@ include_recipe "cookbook_railsbox::locale"
 include_recipe 'apt'
 package "libsqlite3-dev"
 include_recipe 'sqlite'
-if node["cookbook_databox"]["databases"]["mysql"]
-  include_recipe "cookbook_databox::mysql"
-end
-if node["cookbook_databox"]["databases"]["postgresql"]
-  include_recipe "cookbook_databox::postgresql"
-  include_recipe 'cookbook_railsbox::postpostgresql'
-end
+
 include_recipe 'cookbook_rackbox'
 include_recipe 'imagemagick'
